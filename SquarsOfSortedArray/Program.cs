efa8ed4de9dd7252c4c75e9ba9fcaf8d4afc52f3
mohamedabotir <mohamedabotyear@gmail.com>
2022-06-14 19:30:34 +0200

    SquarsOfSortedArray

---
+++ SquarsOfSortedArray/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int[] data = { -7, -3, 2, 3, 11 };
+foreach (var item in SortedSquares(data))
+{
+    Console.WriteLine(item);
+}
+int[] SortedSquares(int[] nums)
+{
+    int[] data = new int[nums.Length];
+    for (int i = 0; i < data.Length; i++)
+    {
+        data[i] = (int)Math.Pow((double)nums[i], 2);
+
+    }
+    return round(data);
+}
+int[] round(int[] nums)
+{
+    int min;
+    for (int i = 0; i < nums.Length - 1; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (nums[i] > nums[j])
+            {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+        }
+    }
+    return nums;
+}
