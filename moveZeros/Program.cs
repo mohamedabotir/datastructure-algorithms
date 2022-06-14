b373575dc6f8fc3245cb3ec2951cbfa09cc05388
mohamedabotir <mohamedabotyear@gmail.com>
2022-06-14 22:36:09 +0200

    MoveZeros

---
+++ moveZeros/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] data = { 0, 1, 0, 3, 12 };
+MoveZeroes(data);
+void MoveZeroes(int[] nums)
+{
+    int i = 0;
+    foreach (var item in nums)
+    {
+        if (item != 0)
+        {
+            nums[i] = item;
+            i++;
+            Console.WriteLine(i + " " + item);
+        }
+    }
+    while (i < nums.Length)
+    {
+        nums[i++] = 0;
+    }
+}
