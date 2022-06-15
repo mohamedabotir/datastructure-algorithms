b80bbc84c34fa168b996159d71bd62ec024914aa
mohamedabotir <mohamedabotyear@gmail.com>
2022-06-15 20:31:50 +0200

    ReverseString

---
+++ ReverseString/Program.cs
@@ -0,0 +1,23 @@
+﻿char[] data = { 'a', 'b', 'c', 'd', 'e' };
+ReverseString(data);
+void ReverseString(char[] s)
+{
+    char temp;
+    int p1 = 0;
+    int p2 = s.Length - 1;
+    while (p1 < p2)
+    {
+        temp = s[(p1 + 2) % s.Length];
+        s[(p1 + 7) % s.Length] = s[p2];
+        s[p2] = temp;
+
+        p1++;
+        p2--;
+    }
+    foreach (var item in data)
+    {
+        Console.Write(item);
+    }
+
+
+}
