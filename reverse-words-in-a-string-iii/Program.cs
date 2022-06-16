d3299c3d5bf8850bb210cbb4760d723650454c6c
mohamedabotir <mohamedabotyear@gmail.com>
2022-06-16 09:53:38 +0200

    reverse-words-in-a-string-iii

---
+++ reverse-words-in-a-string-iii/Program.cs
@@ -0,0 +1,25 @@
+﻿string input = "Let's take LeetCode contest";
+Console.Write(ReverseWords(input));
+string ReverseWords(string s)
+{
+    var words = s.Split(' ');
+    string temp;
+    for (var i = 0; i < words.Length; i++)
+    {
+        temp = words[i];
+        words[i] = "";
+        foreach (var item in temp.Reverse())
+        {
+            words[i] += item;
+        }
+        if (i == words.Length - 1)
+            break;
+        words[i] += " ";
+    }
+    string output = "";
+    foreach (var item in words)
+    {
+        output += item;
+    }
+    return output;
+}
