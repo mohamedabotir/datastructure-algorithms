b5446ede8ef466481a3071bc485a5a0bd2136c27
mohamedabotir <mohamedabotyear@gmail.com>
2022-06-17 22:16:28 +0200

    longest-substring-without-repeating-characters

---
+++ longest-substring-without-repeating-characters/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine(LengthOfLongestSubstring("dvdf"));
+int LengthOfLongestSubstring(string s)
+{
+    int max = 0;
+    int p1 = 0;
+    int p2 = 0;
+    Dictionary<char, int> data = new Dictionary<char, int>();
+    while (p1 < s.Length)
+    {
+        if (!data.ContainsKey(s[p1]))
+        {
+            data.Add(s[p1], p1);
+            p1++;
+            max = Math.Max(max, data.Count);
+        }
+        else
+        {
+            data.Remove(s[p2]);
+            p2++;
+        }
+    }
+
+
+    return max;
+}
