887d6223f121f318a72754fb6e54daf63612a0d6
mohamedabotir <mohamedabotyear@gmail.com>
2022-06-17 04:38:53 +0200

    solve remove nth node issue

--- remove-nth-node-from-end-of-list/Program.cs
+++ remove-nth-node-from-end-of-list/Program.cs
@@ -1,22 +1,23 @@
 ﻿var node = new ListNode(1);
 var node1 = new ListNode(2);
 var node2 = new ListNode(3);
+node.next = node1;
+node1.next = node2;
+/*
 var node3 = new ListNode(4);
 var node4 = new ListNode(5);
 var node5 = new ListNode(6);
-node.next = node1;
-node1.next = node2;
 node2.next = node3;
 node3.next = node4;
 node4.next = node5;
-var data = ListNode.RemoveNthFromEnd(node, 2);
+*/
+var data = ListNode.RemoveNthFromEnd(node, 3);
 //Console.WriteLine(data.val);
 
 Console.WriteLine(data.val);
 Console.WriteLine(data.next.val);
 Console.WriteLine(data.next.next.val);
-Console.WriteLine(data.next.next.next.val);
-Console.WriteLine(data.next.next.next.next.val);
+
 class ListNode
 {
     public int val;
@@ -28,7 +29,7 @@ class ListNode
     }
     public static ListNode RemoveNthFromEnd(ListNode head, int n)
     {
-        int size = 0;
+        int size = 1;
         int p1 = 0;
         int target = 0;
         ListNode temp = head;
@@ -41,25 +42,36 @@ class ListNode
             temp = temp.next;
         }
         target = size - n + 1;
+
+
         if (size == 1)
         {
-            return null;
+            temp = null;
         }
-        temp = head;
-        while (head.next != null)
+        else
         {
-            p1++;
-            if (head.next == null)
-                break;
-            head = head.next;
-            if (p1 == target - 1)
+            if (size == n)
             {
-                head.next = head.next.next;
-                head.next.next = null;
-                break;
+                head = head.next;
+                return head;
+            }
+            temp = head;
+
+            while (head.next != null)
+            {
+                p1++;
+                if (head.next == null)
+                    break;
+
+                if (p1 == target - 1)
+                {
+                    head.next = head.next.next;
+                    break;
+
+                }
+                head = head.next;
 
             }
-
         }
 
 
