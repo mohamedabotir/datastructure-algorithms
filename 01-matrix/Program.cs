cf34d9eb5f57bbbb4be0bef7c1acd2e6abddfb91
mohamedabotir <mohamedabotyear@gmail.com>
2022-06-25 00:52:18 +0200

    Merge branch 'main' of https://github.com/mohamedabotir/datastructure-algorithms

--- 01-matrix/Program.cs
+++ 01-matrix/Program.cs
@@ -1,5 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
- int[][] UpdateMatrix(int[][] mat) {
-        
+﻿int m;
+int n;
+int max = 0;
+bool isFound = false;
+bool start = false;
+int[][] UpdateMatrix(int[][] mat)
+{
+    m = mat.Length;
+    n = mat[0].Length;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (mat[i][j] == 1)
+            {
+
+                start = true;
+                recurse(i, j, mat);
+                mat[i][j] = max;
+                max = 0;
+                isFound = false;
+                start = false;
+            }
+        }
     }
+    return mat;
+}
+void recurse(int x, int y, int[][] mat)
+{
+    if (x >= m || x < 0 || y >= n || y < 0)
+    {
+        return;
+    }
+
+    if (isFound)
+        return;
+    if (mat[x][y] == 0 && !isFound)
+    {
+        Console.WriteLine(mat[x][y]);
+        max++;
+        isFound = true;
+        return;
+    }
+    else if (start)
+    {
+        max = 0;
+        start = false;
+    }
+    else
+    {
+        max++;
+    }
+    recurse(x - 1, y, mat);
+    recurse(x + 1, y, mat);
+    recurse(x, y - 1, mat);
+    recurse(x, y + 1, mat);
+
+
+
+}
