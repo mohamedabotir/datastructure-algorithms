bd270364df7aab8c10c3e9f114a8c05d7c8def85
mohamedabotir <mohamedabotyear@gmail.com>
2022-06-25 15:13:53 +0200

    combination,permiutation,LetterCasePermiutation

---
+++ LetterCasePermiutation/Program.cs
@@ -0,0 +1,31 @@
+﻿IList<string> LetterCasePermutation(string s)
+{
+    var rtn = new List<string>();
+    var chars = s.ToCharArray();
+    Generate(rtn, chars, 0);
+    return rtn;
+}
+
+void Generate(List<string> set, char[] s, int index)
+{
+    set.Add(new string(s));
+    for (int i = index; i < s.Length; i++)
+    {
+        if (Char.IsLetter(s[i]))
+        {
+            s[i] = Convert(s[i]);
+            Generate(set, s, i + 1);
+            //Restore
+            s[i] = Convert(s[i]);
+        }
+    }
+}
+
+char Convert(char c)
+{
+    if (Char.IsLower(c))
+    {
+        return Char.ToUpper(c);
+    }
+    return Char.ToLower(c);
+}
