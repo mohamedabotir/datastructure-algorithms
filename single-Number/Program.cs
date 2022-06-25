aa900bf6032d0a2dc953902d200a4241b09f503a
mohamedabotir <mohamedabotyear@gmail.com>
2022-06-25 16:33:21 +0200

    climbingStairs, House Robber,Triangle,Power of Two,Number of 1 Bits,Reverse Bits,Single Number

---
+++ single-Number/Program.cs
@@ -0,0 +1,12 @@
+﻿int SingleNumber(int[] nums) {
+          var dict = new Dictionary<int,int>();
+        for(var i=0;i<nums.Length;i++)
+        {
+            if(dict.ContainsKey(nums[i]))  
+                dict.Remove(nums[i]);
+            else 
+                dict.Add(nums[i],nums[i]); 
+        }
+        foreach(var i in dict.Keys)  return i; 
+        return int.MaxValue;
+    }
