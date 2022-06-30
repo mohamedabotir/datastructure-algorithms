bf8c88b0b256be23f5aa317b5a82297b248ae973
mohamedabotir <mohamedabotyear@gmail.com>
2022-06-30 12:08:40 +0200

    minimum-moves-to-equal-array-elements-ii

---
+++ minimum-moves-to-equal-array-elements-ii/Program.cs
@@ -0,0 +1,12 @@
+﻿ int MinMoves2(int[] nums) {
+        
+        
+        
+        int steps = 0;
+        Array.Sort(nums);
+        int mid = nums[nums.Length/2];
+            for(int i=0;i<nums.Length;i++){
+                steps+=Math.Abs(nums[i]-mid);
+            }
+        return steps;
+    }
