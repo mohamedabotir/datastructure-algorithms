b25b74b79850d7628a4d89a4b70a3594b6a9938f
mohamedabotir <mohamedabotyear@gmail.com>
2022-08-06 12:30:13 +0200

    pivotIndex

---
+++ findpivotindex/Program.cs
@@ -0,0 +1,13 @@
+﻿int PivotIndex(int[] nums)
+{
+    int sum = 0;
+    int leftSum = 0;
+    foreach (var num in nums)
+        sum += num;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (leftSum == sum - leftSum - nums[i]) return i;
+        leftSum += nums[i];
+    }
+    return -1;
+}
