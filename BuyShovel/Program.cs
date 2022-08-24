5e07cfd6e6b3df12aa95f87ecc1041c6511813e6
mohamedabotir <mohamedabotyear@gmail.com>
2022-08-24 15:32:42 +0200

    BuyShovel

---
+++ BuyShovel/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Split(' ');
+        int i = 0;
+        for (i = 1; true; i++)
+        {
+
+            if ((i * int.Parse(input[0]) % 10) == int.Parse(input[1]) || (i * int.Parse(input[0]) % 10) == 0)
+            {
+                break;
+            }
+        }
+        Console.WriteLine(i);
+    }
+}
