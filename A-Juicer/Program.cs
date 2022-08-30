4f993145744d92247cb501b62d6c447208014900
mohamedabotir <mohamedabotyear@gmail.com>
2022-08-30 21:35:08 +0200

    A-Juicer

---
+++ A-Juicer/Program.cs
@@ -0,0 +1,35 @@
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Split(" ");
+        var orangesSize = Console.ReadLine().Split(" ");
+        int ornagesNum = int.Parse(input[0]);
+        int maxOrangeSize = int.Parse(input[1]);
+        int maxJuicerTank = int.Parse(input[2]);
+        int emptiesTankNumber = 0;
+        int currentTank = 0;
+        for (int i = 0; i < ornagesNum; i++)
+        {
+            if (maxOrangeSize < int.Parse(orangesSize[i]))
+                continue;
+            if ((currentTank + int.Parse(orangesSize[i]) <= maxJuicerTank))
+            {
+                currentTank += int.Parse(orangesSize[i]);
+            }
+            else
+            {
+                emptiesTankNumber++;
+                currentTank = 0;
+            }
+
+        }
+        Console.WriteLine(emptiesTankNumber);
+
+    }
+}
---
+++ horseshoe/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Split(" ");
+
+        var items = new Dictionary<int, int>();
+        items.Add(int.Parse(input[0]), 1);
+        for (var i = 1; i < input.Length; i++)
+        {
+            var converter = int.Parse(input[i]);
+
+            if (!items.ContainsKey(converter))
+            {
+                items.Add(converter, 1);
+            }
+        }
+        Console.WriteLine(4 - items.Count());
+    }
+}
