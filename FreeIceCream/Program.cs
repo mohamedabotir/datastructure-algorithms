cff4208c344f560fc7d2e79422c1bf1e1f09c8ee
mohamedabotir <mohamedabotyear@gmail.com>
2022-09-19 12:49:05 +0200

    FreeIceCream , WayTooLongWords

---
+++ FreeIceCream/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Split(" ");
+        var input1 = int.Parse(input[0]);
+        var input2 = long.Parse(input[1]);//initialize ice value
+        var distress = 0;
+        for (var i = 0; i < input1; i++)
+        {
+            var carrier = Console.ReadLine();
+            var split = carrier.Split(' ');
+            if (split[0] == "+")
+            {
+                input2 += int.Parse(split[1]);
+            }
+            else if (split[0] == "-")
+            {
+                if (input2 >= int.Parse(split[1]))
+                    input2 -= int.Parse(split[1]);
+                else
+                    distress++;
+            }
+        }
+        Console.WriteLine(input2 + " " + distress);
+    }
+}
---
+++ WayTooLongWords/Program.cs
@@ -0,0 +1,18 @@
+﻿var input = Console.ReadLine();
+var lines = int.Parse(input);
+var words = new string[lines];
+for (var i = 0; i < lines; i++)
+{
+    words[i] = Console.ReadLine();
+}
+
+for (var i = 0; i < lines; i++)
+{
+    if (words[i].Length > 10)
+        words[i] = words[i].ToCharArray()[0].ToString() + (words[i].Length - 2) + words[i].ToCharArray()[words[i].Length - 1];
+}
+
+foreach (var item in words)
+{
+    Console.WriteLine(item);
+}
